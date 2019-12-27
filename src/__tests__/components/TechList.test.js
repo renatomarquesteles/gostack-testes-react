d9f87b58f72cf98db50399035a5500c420f51838
Renato <renatomarquesteles@gmail.com>
2019-12-27 15:44:42 -0300

    mock do localstorage

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 
 import TechList from "~/components/TechList";
 
 describe("TechList component", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
   it("should be able to add a new tech", () => {
     const { getByText, getByTestId, getByLabelText, debug } = render(
       <TechList />
     );
 
-    debug();
+    // debug();
 
     fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
     fireEvent.submit(getByTestId("tech-form"));
 
-    debug();
+    // debug();
 
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
     expect(getByLabelText("Tech")).toHaveValue("");
+  });
+
+  it("it should store techs in storage", () => {
+    let { getByTestId, getByLabelText, getByText } = render(<TechList />);
+
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
+    fireEvent.submit(getByTestId("tech-form"));
+
+    cleanup();
+
+    ({ getByTestId, getByLabelText, getByText } = render(<TechList />));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "techs",
+      JSON.stringify(["Node.js"])
+    );
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
   });
 });
