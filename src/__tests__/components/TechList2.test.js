7adc48a2a675e19a19bdcaca60a55ee69e6cee4d
Renato <renatomarquesteles@gmail.com>
2019-12-27 16:00:29 -0300

    mock do useselector do react redux

---
+++ src/__tests__/components/TechList2.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { render } from "@testing-library/react";
+
+import TechList from "~/components/TechList2";
+
+jest.mock("react-redux");
+
+describe("TechList component", () => {
+  it("should render tech list", () => {
+    useSelector.mockImplementation(cb =>
+      cb({
+        techs: ["Node.js", "ReactJS"]
+      })
+    );
+
+    const { getByTestId, getByText } = render(<TechList />);
+
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+    expect(getByTestId("tech-list")).toContainElement(getByText("ReactJS"));
+  });
+});
---
+++ src/components/TechList2/index.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+// import { Container } from './styles';
+
+export default function TechList() {
+  const [newTech, setNewTech] = useState("");
+
+  const dispatch = useDispatch();
+  const techs = useSelector(state => state.techs);
+
+  function handleAddTech() {
+    dispatch({ type: "ADD_TECH", payload: { tech: newTech } });
+    setNewTech("");
+  }
+
+  return (
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
+      <ul data-testid="tech-list">
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input
+        id="tech"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+
+      <button onClick={handleAddTech}>Adicionar</button>
+    </form>
+  );
+}
